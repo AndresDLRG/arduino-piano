11d56d5e9e3fa7c83f053b6c53c02e5249e6d486
Horacio Garza <hgx95@hotmail.com>
2016-05-05 10:55:00 -0500

    Updated
    Updated but it isn't deployed.

--- learning_mode/Proyecto.ino
+++ learning_mode/Proyecto.ino
@@ -8,6 +8,8 @@ uint8_t LEDS[8] = {9,10,11,12,13,14,15,16};
 
 int nobodyGotIt = 999;
 int steps[8] = {8,4,2,1,5,3,7,6};
+int stepNumber;
+boolean finished = false;
 
 void setup() {
 
@@ -19,14 +21,21 @@ void setup() {
   for(int i = 0 ; i < sizeof(LEDS) ; i++){
     pinMode(LEDS[i], OUTPUT);
   }
+  
+  stepNumber = 0;
 
 }
 
 
 void loop() {
 
+  digitalWrite(LEDS[stepNumber], 1);
   uint8_t whoGotTheP = getPressedButtonID();
+  
   letTheLEDWakeUp(whoGotTheP);
+  if(finished){
+    learnFinishedAnimation();
+  }
   
 }
 
@@ -40,18 +49,45 @@ int getPressedButtonID(){
       return i;
     }
   }
-  
   return nobodyGotIt;
-    
 
 }
 
 void letTheLEDWakeUp(int pint){
 
-  if(pint != nobodyGotIt){
-    digitalWrite(LEDS[pint], !digitalRead(LEDS[pint]));
+  if(LEDS[pint] == steps[stepNumber]){
+
+    digitalWrite(LEDS[stepNumber], 0);
+    if(stepNumber == sizeof(steps)){
+      finished = true
+      
+    }else{
+      stepNumber++;
+      digitalWrite(LEDS[stepNumber], 1);
+      
+    }
+    
+    
+  }else{
+    
+    digitalWrite(LEDS[stepNumber], 0);
+    delay(250);
+    digitalWrite(LEDS[stepNumber], 1);
+    delay(250);
+    digitalWrite(LEDS[stepNumber], 0);
+    delay(250);
+    digitalWrite(LEDS[stepNumber], 1);
+    delay(100);
   }
 
+}
+
+void learnFinishedAnimation(){
+  for(int i = 0 ; i < (sizeof(LEDS) - 1) ; i++){
+    digitalWrite(LEDS[i], 1);
+    delay(200);
+    digitalWrite(LEDS[i], 0);
+  }
 }
 
 
